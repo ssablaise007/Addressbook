0ec31a0fe2d633788c396b5ac76dc378ca316ea8
Your Name <you@example.com>
2021-07-27 17:52:50 +0530

    Added : Removing Contact

---
+++ contactDetail.java
@@ -0,0 +1,9 @@
+package ContactAddreesBook;
+
+public class contactDetail {
+
+	public contactDetail(String firstname, String lastname, String address, String city, String state, int zip,
+			long phonenumber, String email) {
+	}
+
+}
